a38e08aa029780b1f2af6a1971d42e301010586f
Abdus Quadri <asquadri1@gmail.com>
2025-04-30 15:07:07 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+api_key = "api-key-here"
+api_secret = "api-secret-here"
+
+url = "https://api2.prismacloud.io/" # prisma cloud url
+
+compute_url = "https://us-east1.cloud.twistlock.com/us-xxxxxxx/" # compute url
